7bf3b709a96e64e6611f6236ccbadf5c2d7aa0c2
Duane <duane.edmonds@gmail.com>
2022-08-07 11:40:54 -0700

    (automated gcp push)

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,33 @@
+use std::env;
+
+const RELEASE_STATE: &str = "a";
+
 fn main() {
+    // enable or disable backtrace on error
+    env::set_var("RUST_BACKTRACE", "0");
+
+    // get command arguments
+    let mut args: Vec<String> = env::args().collect();
+
+    // if no arguments are passed, behave as if help flag was passed
+    if args.len() <= 1 {
+        args.push("help".to_string());
+    }
+
     println!("hola");
+    println!("{:#?}", args);
+
+    match args[1].as_str() {
+        "--help" | "help" => {
+            // display command usage information
+            show_help();
+            return;
+        }
+        _ => ()
+    }
+
+}
+
+fn show_help() {
+    //TODO
 }
