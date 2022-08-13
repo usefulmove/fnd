256a99fbf9a3ad437b168723218c7347cdda9283
Duane <duane.edmonds@gmail.com>
2022-08-12 20:54:52 -0700

    (automated gcp push)

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
+use::colored::*;
 use std::env;
+use std::fs;
+use std::path::Path;
 
 const RELEASE_STATE: &str = "a";
 
@@ -9,12 +12,11 @@ fn main() {
     // get command arguments
     let mut args: Vec<String> = env::args().collect();
 
-    // if no arguments are passed, behave as if help flag was passed
+    // if no arguments are passed, behave as if help flag was passed : TODO - remove to allow "fnd" same as "fnd ."?
     if args.len() <= 1 {
         args.push("help".to_string());
     }
 
-    println!("hola");
     println!("{:#?}", args);
 
     match args[1].as_str() {
@@ -23,11 +25,22 @@ fn main() {
             show_help();
             return;
         }
+        "--version" | "version" => {
+            // display version information
+            println!("{}{}", env!("CARGO_PKG_VERSION"), RELEASE_STATE);
+            return;
+        }
         _ => ()
     }
+
+    // 
+
+    println!("execute fnd here: ( % fnd {} )", args[1]);
+
 
 }
 
 fn show_help() {
     //TODO
+    println!("\"( oh! hola. need some halp? )\"");
 }
