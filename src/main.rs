41eba5bca755195f71a24a6f415ff9d363a91989
Duane <duane.edmonds@gmail.com>
2022-08-14 01:51:43 -0700

    completed initial pass at basic output formatting

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
+use colored::*;
 use std::env;
 use std::path::Path;
 use walkdir::{WalkDir, DirEntry};
+
+mod poc;
 
 const RELEASE_STATE: &str = "b";
 
@@ -47,8 +50,8 @@ fn main() {
         let path: &Path = entry.path();
         let path_str: &str = path.to_str().unwrap();
         if path_str.contains(&search_term) {
-            println!("  {}", path_str);
-            println!("  {:#?}", path_str.to_string().split(&search_term).collect::<Vec<&str>>());
+            //println!("  {}", path_str); // debug
+            output_highlight(path_str, &search_term);
         }
     }
 
@@ -58,4 +61,38 @@ fn main() {
 fn show_help() {
     //TODO
     println!("  ( oh! hola. need halp? )");
+}
+
+fn output_highlight(output_string: &str, highlight_term: &str) {
+    /* find the highlight term in the output string and format the output 
+     * string to emphasize the highlight term in the output string
+     */
+
+    //println!("  {}", output_string); // debug
+
+    //println!("  {:#?}", path_str.to_string().split(&search_term).collect::<Vec<&str>>());
+
+    let o: String = output_string.clone().to_string();
+    let elements: Vec<&str> = o.split(&highlight_term).collect::<Vec<&str>>();
+
+    //print!("{:#?}", elements); // debug
+
+    // print highlighted output string
+    print!("  ");
+    for i in 0..elements.len() {
+        if i < (elements.len() - 1) {
+            print!(
+                "{}{}",
+                poc::color_grey_mouse(elements[i]),
+                poc::color_blue_smurf_bold(highlight_term),
+            );
+        } else {
+            print!(
+                "{}",
+                poc::color_grey_mouse(elements[i]),
+            );
+        }
+    }
+    println!("");
+
 }
