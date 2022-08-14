f243e96cc73caf0b11056c3234d7ff8fa787c16a
Duane <duane.edmonds@gmail.com>
2022-08-13 23:10:39 -0700

    updated main function

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,27 @@
-use::colored::*;
 use std::env;
-use std::fs;
 use std::path::Path;
-use walkdir::WalkDir;
+use walkdir::{WalkDir, DirEntry};
 
-const RELEASE_STATE: &str = "a";
+const RELEASE_STATE: &str = "b";
 
 fn main() {
     // enable or disable backtrace on error
     env::set_var("RUST_BACKTRACE", "0");
 
     // get command arguments
-    let mut args: Vec<String> = env::args().collect();
+    let args: Vec<String> = env::args().collect();
 
     //println!("{:#?}", args); // debug
+
+    if args.len() <= 1 {
+        for entry in WalkDir::new("./") {
+            let entry: DirEntry = entry.unwrap();
+            let path: &Path = entry.path();
+            let path_str: &str = path.to_str().unwrap();
+            println!("{}", path_str);
+        }
+        return;
+    }
 
     match args[1].as_str() {
         "--help" | "help" => {
@@ -29,15 +37,15 @@ fn main() {
         _ => ()
     }
 
-    // execute fnd core
+    /* fnd core */
 
     // 0.2.0 - simple search
     let search_term: String = args[1].clone();
 
     for entry in WalkDir::new("./") {
-        let entry = entry.unwrap();
-        let path = entry.path();
-        let path_str = path.to_str().unwrap();
+        let entry: DirEntry = entry.unwrap();
+        let path: &Path = entry.path();
+        let path_str: &str = path.to_str().unwrap();
         if path_str.contains(&search_term) {
             println!("{}", path_str);
         }
@@ -47,5 +55,5 @@ fn main() {
 
 fn show_help() {
     //TODO
-    println!("  ( oh! hola. need some halp? )");
+    println!("  ( oh! hola. need halp? )");
 }
