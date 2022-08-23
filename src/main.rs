f34c7991385dc4304560324c07387d05986032c2
Duane <duane.edmonds@gmail.com>
2022-08-22 21:04:35 -0700

    added verbose option; updated argument handling; added option to set search directory

--- src/main.rs
+++ src/main.rs
@@ -14,62 +14,109 @@ fn main() {
     // set default search mode
     let mode: SearchMode = SearchMode::Regex;
 
+    // set default search directory
+    let mut dir: String = "./".to_string();
+
+    // set default verbosity
+    let mut verbose: bool = false;
+
     // don't show hidden files by default
     let mut ignore_hidden: bool = true;
 
     // get command arguments
     let mut args: Vec<String> = env::args().collect();
 
-    //println!("{:#?}", args); // debug
-    let col = poc::Theme::new();
+    let color_theme = poc::Theme::new();
+
+    args.remove(0); // remove the program name from arguments list
 
     // if no arguments are passed, behave as if help flag was passed
-    /*
-    if args.len() <= 1 {
+    if args.len() < 1 {
         args.push("--help".to_string());
     }
-    */
 
-    match args[1].as_str() {
-        "--help" => {
-            // display command usage information
-            show_help();
-            return;
-        }
-        "--hidden" | "-h" => {
-            // show hidden files
-            ignore_hidden = false;
+    let mut search_expr: String = "".to_string();
+    while args.len() >= 1 {
+        match args[0].as_str() {
+            "--dir" | "-d" => {
+                // set search directory
+                args.remove(0); // remove flag
+                dir = args.remove(0); // save directory argument
 
-            // remove flag argument
-            args.remove(1);
+                if verbose {
+                    println!(
+                        "  {}",
+                        color_theme.color_rgb("  verbose mode", &color_theme.orange_sherbet_bold),
+                    );
+                }
+            }
+            "--help" => {
+                // display command usage information
+                show_help();
+                return;
+            }
+            "--hidden" | "-h" => {
+                args.remove(0); // remove flag
+                ignore_hidden = false; // show hidden files
+                if verbose {
+                    println!(
+                        "  {}",
+                        color_theme.color_rgb("  searching hidden files", &color_theme.orange_sherbet_bold),
+                    );
+                }
+            }
+            "--version" => {
+                // display version information
+                println!(
+                    "  {} {}{}",
+                    color_theme.color_rgb("fnd", &color_theme.grey_mouse),
+                    color_theme.color_rgb(env!("CARGO_PKG_VERSION"), &color_theme.blue_smurf_bold),
+                    color_theme.color_rgb(RELEASE_STATE, &color_theme.white_bold),
+                );
+                return;
+            }
+            "--verbose" | "-v" => {
+                args.remove(0); // remove flag
+                println!(
+                    "  {}",
+                    color_theme.color_rgb("  verbose mode", &color_theme.orange_sherbet_bold),
+                );
+                verbose = true;
+            }
+            _ => {
+                if args.len() > 1 {
+                    eprintln!(
+                        "  {}: incorrect use of command arguments",
+                        color_theme.color_rgb("error", &color_theme.red_bold),
+                    );
+                    std::process::exit(1);
+                }
+                if args.len() == 1 {
+                    search_expr = args.remove(0); // get search expression
+
+                    if verbose {
+                        println!(
+                            "  search expression is \"{}\"",
+                            color_theme.color_rgb(&search_expr, &color_theme.blue_smurf_bold),
+                        );
+                    }
+                }
+            }
         }
-        "--version" => {
-            // display version information
-            println!(
-                "  {} {}{}",
-                col.color_rgb("fnd", &col.grey_mouse),
-                col.color_rgb(env!("CARGO_PKG_VERSION"), &col.blue_smurf_bold),
-                col.color_rgb(RELEASE_STATE, &col.white_bold),
-            );
-            return;
-        }
-        _ => ()
     }
 
     /* fnd core */
     match mode {
         SearchMode::Regex => {
             /* regular expression ( regex ) search */
-            let reg_exp: String = args[1].clone();
+            let reg_exp: String = search_expr;
 
             match Regex::new(&reg_exp) {
                 Ok(re) => {
-                    for entry in WalkBuilder::new("./").hidden(ignore_hidden).build() {
+                    for entry in WalkBuilder::new(&dir).hidden(ignore_hidden).build() {
                         let entry: DirEntry = entry.unwrap();
                         let path: &Path = entry.path();
                         let path_str: &str = path.to_str().unwrap();
-
-                        //println!("  debug:path:{}", path_str);
 
                         let no_match: String = "".to_string();
 
@@ -79,40 +126,42 @@ fn main() {
                         };
 
                         if matching_term != no_match {
-                            //println!("  debug:{}", col.color_rgb(path_str, &col.blue_smurf_bold));
                             println!(
                                 "  {}",
                                 poc::highlight(
                                     path_str,
                                     &matching_term,
-                                    &col.blue_smurf_bold,
+                                    &color_theme.blue_smurf_bold,
                                 ),
                             );
                         }
                     }
                 }
                 Err(e) => {
-                    println!("  error!: {}", e);
+                    println!(
+                        "  {}: {}",
+                        color_theme.color_rgb("error", &color_theme.red_bold),
+                         e,
+                    );
                 }
             }
         }
         /*
         SearchMode::Simple => {
             /* simple search */
-            let search_term: String = args[1].clone();
+            let search_term: String = args[0].clone();
 
             for entry in WalkBuilder::new("./").hidden(ignore_hidden).build() {
                 let entry: DirEntry = entry.unwrap();
                 let path: &Path = entry.path();
                 let path_str: &str = path.to_str().unwrap();
                 if path_str.contains(&search_term) {
-                    //println!("  {}", path_str); // debug
                     println!(
                         "  {}",
                         poc::highlight(
                             path_str,
                             &search_term,
-                            &col.blue_smurf_bold,
+                            &color_theme.blue_smurf_bold,
                         ),
                     );
                 }
